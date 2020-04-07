fc9bf18556004f6b9554993737fe2a42de32a273
Andre Carter <carstar2486@gmail.com>
2020-04-07 02:41:55 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,5 @@
 def find_element_index(array, value_to_find)
   x = 0
   while x < array.size do
-    if array[x] === value_to_find {
-      return array[x]
-    } else 
-      return nil
+    
 end
